c08bc031f5142a528b3cf5fe16418983fdf90975
Jeremy Villa <villajeremy1991@gmail.com>
2023-04-27 21:35:40 -0400

    Initial commit

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  //selector: 'app-servers',
+  //selector: '[app-servers]', // Example 1, same as above but in an attribute form
+  selector: '.app-servers', // Example 2, same as above but in an class form
+  templateUrl: './servers.component.html', // ALL COMPONENTS NEEDS TO HAVE TEMPLATES AT ALL TIMES
+  // template: '<app-server></app-server><app-server></app-server>', // Ommitted the URL in the key, same thing as what's in the html file
+  // template: `<app-server></app-server>
+  // <app-server></app-server>`, // For multiple lines, using the back ticks
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+  allowNewServer = false;
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 5000);
+  }
+
+  ngOnInit(): void {
+  }
+
+
+
+}
---
+++ src/app/success-alert/success-alert.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-success-alert',
+  templateUrl: './success-alert.component.html',
+  styleUrls: ['./success-alert.component.css']
+})
+export class SuccessAlertComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  displaySuccessAlert() {
+    alert("Great Success!");
+  }
+
+}
